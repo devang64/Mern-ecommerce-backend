74164a4aec36e88e09f732348ac79a8eebfa7f83
devang64 <pateldevang6432@gmail.com>
2024-02-22 22:07:12 +0530

    index file update

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const { PORT } = require('./config');
+const connectDataBase = require('./db');
+const cloudinary = require('cloudinary');
 const express = require("express");
 const app = express();
 const product = require("./router/productRoute");
@@ -10,21 +13,28 @@ var cors = require('cors');
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const path = require('path')
+connectDataBase();
 
 
-app.use(bodyParser.urlencoded({extended :true} ))
-app.use(fileUpload({useTempFiles:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(fileUpload({ useTempFiles: true }))
 
-app.use(cors()) 
+app.use(cors())
 app.use(express.json());
 app.use(cookieParser())
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", orderRoutes);
 app.use("/api/v1", payment);
-
-
 // middleware for error
 app.use(errorMiddleware);
 
-module.exports = app;
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on http://localhost:${PORT}`)
+})
