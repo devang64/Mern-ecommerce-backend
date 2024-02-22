158bb15d9ef421d6b01d28089fcbcf0690d8d8c7
devang64 <pateldevang6432@gmail.com>
2024-02-22 22:24:12 +0530

    update files

--- index.js
+++ index.js
@@ -1,38 +1,15 @@
-const { PORT } = require('./config');
+const app = require('./app');
+const { PORT, CLOUDINARY_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } = require('./config');
 const connectDataBase = require('./db');
 const cloudinary = require('cloudinary');
-const express = require("express");
-const app = express();
-const product = require("./router/productRoute");
-const errorMiddleware = require("./Middleware/Error");
-const user = require("./router/userRoutes");
-const cookieParser = require("cookie-parser");
-const orderRoutes = require('./router/orderRoute')
-const payment = require('./router/paymentRoute')
-var cors = require('cors');
-const bodyParser = require("body-parser");
-const fileUpload = require("express-fileupload");
-const path = require('path')
+
+
 connectDataBase();
 
-
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(fileUpload({ useTempFiles: true }))
-
-app.use(cors())
-app.use(express.json());
-app.use(cookieParser())
-app.use("/api/v1", product);
-app.use("/api/v1", user);
-app.use("/api/v1", orderRoutes);
-app.use("/api/v1", payment);
-// middleware for error
-app.use(errorMiddleware);
-
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: CLOUDINARY_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
 });
 
 app.listen(PORT, () => {
