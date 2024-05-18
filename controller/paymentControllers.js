30c576cb2ea7b6857f1c0d58db70ee1edbea3511
devang64 <pateldevang6432@gmail.com>
2024-05-18 10:14:59 +0530

    Update paymentControllers.js

--- controller/paymentControllers.js
+++ controller/paymentControllers.js
@@ -1,24 +1,25 @@
-const stripe = require("stripe");
-const {STRIPE_API_KEY} = require('../config')
+const { STRIPE_API_KEY,STRIPE_SECRET_KEY } = require('../config')
+const stripe = require("stripe")(STRIPE_SECRET_KEY);
 
 exports.processPayment = async (req, res, next) => {
-    const myPayment = await stripe.paymentIntents.create({
-      amount: req.body.amount,
-      currency: "inr",
-      metadata: {
-        company: "Ecommerce",
-      },
-      automatic_payment_methods: {
-        enabled: true,
-      },
-      payment_method: 'pm_card_visa',
-    });
-  
-    res
-      .status(200)
-      .send({client_secret: myPayment.client_secret});
-  };
-  
-  exports.sendStripeApiKey = async (req, res, next) => {
-    res.status(200).json({ stripeApiKey: STRIPE_API_KEY });
-  };
+  const myPayment = await stripe.paymentIntents.create({
+    amount: req.body.amount,
+    currency: "inr",
+    metadata: {
+      company: "Ecommerce",
+    },
+    automatic_payment_methods: {
+      enabled: true,
+    },
+    payment_method: 'pm_card_visa',
+  });
+
+  res
+    .status(200)
+    .send({ client_secret: myPayment.client_secret });
+};
+
+exports.sendStripeApiKey = async (req, res) => {
+
+  res.status(200).json({ stripeApiKey: STRIPE_API_KEY });
+};
